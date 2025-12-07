f41ab234716c6d5e06336d6044024751f931cdcf
simergyHome <10>
2025-12-07 18:36:50 +0200

    Adding CloseAllMenusExceptMe event

--- Runtime/NP_UI_System/Scripts/Menu/Events/NP_EventsManager.cs
+++ Runtime/NP_UI_System/Scripts/Menu/Events/NP_EventsManager.cs
@@ -7,6 +7,7 @@ namespace NP_UI
     {
         public static UnityEvent<NpGenericMenu> CloseMenuEvent = new UnityEvent<NpGenericMenu>();
         public static UnityEvent<NpGenericMenu> OpenMenuEvent = new UnityEvent<NpGenericMenu>();
+        public static UnityEvent<NpGenericMenu> CloseAllMenusExceptMe = new UnityEvent<NpGenericMenu>();
         public static UnityEvent CloseAllMenus = new UnityEvent();
         public static UnityEvent<NpGenericMenu> OnMenuCreated = new UnityEvent<NpGenericMenu>();
     }
